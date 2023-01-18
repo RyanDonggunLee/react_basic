ecddd601d8583e504157f28142fc5f040d6d0035
donggunLee-DG <donggun.lee.01@gmail.com>
2023-01-17 23:22:06 -0600

    JSX and  JS

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Ryan';
+  const list = ['우유','딸기','바나나','요거트']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{`Hello! ${name}`}</h1>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
