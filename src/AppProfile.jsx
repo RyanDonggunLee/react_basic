c9ea679049819b70e4cdbb90ce75fee1f524be1f
RyanDonggunLee <ryandglee@gmail.com>
2023-01-20 11:04:56 -0600

    adding new tag on profile

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -8,16 +8,19 @@ function AppProfile() {
         image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
         name='James Kim'
         title='Frontend Developer'
+        date='20230115'
       />      
       <Profile 
         image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
         name='Anna Young'
         title='Backend Developer'
+        date='20181113'
       />      
       <Profile 
         image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
         name='Bob Yu'
         title='Frontend Developer'
+        date='20090613'
       />
     </>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
-export default function Profile({image, name, title}) {
+export default function Profile(props) {
+    const date = new Date(props.date.substr(0,4),props.date.substr(4,2)-1,props.date.substr(6,2));
+    const today = new Date();
+    console.log(props.name);
+    console.log(date);
+    console.log(today);
+    var isNew = false;
+    if((today.getTime()-date.getTime())/(1000*60*60*24) < 60) {
+        isNew = true;
+    } else {
+        isNew = false;
+    }
     return (
     <div className='profile'>
         <img className='photo'
-            src={image}
+            src={props.image}
             alt='avatar'
         />
-        <h1>{name}</h1>
-        <p>{title}</p>
+        {
+            isNew && <span className ='new'>New</span>
+        }
+        <h1>{props.name}</h1>
+        <p>{props.title}</p>
     </div>
     )
 }
