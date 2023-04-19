52885ad3e2de405d7ab0badc591cfe62b287a042
donggunLee-DG <ryandglee@gmail.com>
2023-04-18 22:54:26 -0500

    test reusable component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AddForm from './AddForm';
-import AppWrap from './AppWrap';
+import AppCard from './AppCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppWrap />
+    <AppCard />
   </React.StrictMode>
 );
 
