bbf56d781be5f6c73fcc7a2696a0bf3141af493e
donggunLee-DG <ryandglee@gmail.com>
2023-04-18 22:39:36 -0500

    test wrap component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AddForm from './AddForm';
+import AppWrap from './AppWrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AddForm />
+    <AppWrap />
   </React.StrictMode>
 );
 
